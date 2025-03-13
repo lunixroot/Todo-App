3247d0f1ff4b5d2d90fd33b83f1b1fb12ce4650d
Adarsh <adarsht740@gmail.com>
2025-03-14 01:01:13 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,65 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todo, settodo] = useState("");
   const [todos, settodos] = useState([]);
+const [finsh, setfinsh] = useState(true);
 
-  const handleEdit = () => {
-    console.log("Edit");
+  useEffect(() => {
+    let todostring = localStorage.getItem("todos");
+    if(todostring){
+      let todos = JSON.parse(todostring);
+      settodos(todos);
+    }
+  }, [])
+  
+  const saveToLocalStorage = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-  const handleDelete = () => {
-    console.log("Delete");
+
+  const togglefinish = () => {
+    setfinsh(!finsh);
+  }
+
+  const handleEdit = (e, id) => {
+     let t = todos.filter( i=>i.id===id)
+     settodo(t[0].todo);
+     let newTodos = todos.filter(item => {
+      return item.id !==id;
+    });
+    settodos(newTodos);
+    saveToLocalStorage();
   };
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter(item => {
+      return item.id !==id;
+    });
+    settodos(newTodos);
+    saveToLocalStorage();
+  };
+
   const handleChange = (e) => {
     settodo(e.target.value);
-    console.log(e.target.value);
-  };
-  const handleAdd = () => {
-    // console.log("Add");
-    console.log(todo);
-    settodos([...todos, { todo, isCompleted: false }]);
-    settodo("");
-    console.log(todos);
+
   };
 
+  const handleAdd = () => {
+    settodos([...todos, {id: uuidv4(), todo, isCompleted: false }]);
+    settodo("");
+    saveToLocalStorage();
+  };
+
+  const handleCheck = (e) => {
+    let id = e.target.name;
+    let newTodos = todos.map((item) => {
+      item.id === id ? (item.isCompleted = !item.isCompleted) : item;
+      return item;
+    });
+    settodos(newTodos);
+    saveToLocalStorage();  }
   return (
     <>
       <Nav />
@@ -39,44 +73,43 @@ function App() {
             type="text"
           />
           <button
-            onClick={handleAdd}
-            className="bg-amber-500 py-1 px-4 hover:bg-amber-600 rounded-md ml-2"
+            onClick={handleAdd} disabled={todo.length<=3}
+            className="bg-amber-500 py-1 disabled:bg-amber-500 px-4 hover:bg-amber-600 rounded-md ml-2"
           >
             Add
           </button>
         </div>
         <div className="mt-5 ">
           <h1 className="font-bold">Your Todo's</h1>
-          <div className="bg-amber-200 w-full p-2 min-h-[70vh] mt-1 border-1">
-            {todos.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="flex w-full bg-indigo-400 flex-wrap justify-between h-16 p-1 border-b-2"
+          <input onChange={togglefinish} type="checkbox" checked={finsh} name="" id="" /> Show Completed Todo's
+          <div className="bg-amber-100 w-full p-2 min-h-[70vh] mt-1 border-1">
+            {todos.map(item => {
+              return (finsh || !item.isCompleted) && <div
+                  key={item.id}
+                  className="flex w-full flex-wrap justify-between items-center p-1 border-b-2"
                 >
-                  <div className="flex gap-3 px-1 w-[75%]  bg-amber-900">
-                    <input type="checkbox" />
+                  <div className="flex gap-3 px-1 max-w-[75%]">
+                    <input type="checkbox" onChange={handleCheck} name={item.id} checked={item.isCompleted} id=""/>
                     <div className={item.isCompleted ? "line-through" : ""}>
                       {item.todo}
                     </div>
                   </div>
                   <div className="flex gap-2">
                     <button
-                      onClick={handleEdit}
-                      className="bg-amber-500 py-1 px-4 hover:bg-amber-600 rounded-md "
+                      onClick={(e)=>handleEdit(e, item.id )}
+                      className="bg-amber-500 h-8 py-1 px-4 hover:bg-amber-600 rounded-md "
                     >
                       Edit
                     </button>
                     <button
-                      onClick={handleDelete}
-                      className="bg-amber-500 py-1 px-4 hover:bg-amber-600 rounded-md "
+                      onClick={(e)=>{handleDelete(e, item.id)}}
+                      className="bg-amber-500 h-8 py-1 px-4 hover:bg-amber-600 rounded-md "
                     >
                       Delete
                     </button>
                   </div>
                 </div>
-              );
-            })}
+            })} 
           </div>
         </div>
       </div>
